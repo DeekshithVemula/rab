272423421f6639b11e990018055ef627c479ad78
DeekshithVemula <48466367+DeekshithVemula@users.noreply.github.com>
2019-08-01 20:14:15 +0530

    Update index.py

--- producer/index.py
+++ producer/index.py
@@ -23,7 +23,7 @@ def hello():
 
     channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 
-    routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
+    routing_key ='anonymous.info'
     message = 'Hello World!'
     channel.basic_publish(
         exchange='topic_logs', routing_key=routing_key, body=message)
