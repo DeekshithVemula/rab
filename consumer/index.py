7798b28f728cc7f98bf613fc1b69eb1f01df05ed
DeekshithVemula <48466367+DeekshithVemula@users.noreply.github.com>
2019-08-06 15:08:27 +0530

    Update index.py

--- consumer/index.py
+++ consumer/index.py
@@ -1,5 +1,3 @@
-import os
-from flask import Flask
 import pika
 import time
 import sys
@@ -18,8 +16,13 @@ application = Flask(__name__)
 
 mi='aka'
 
-@application.route("/")
+@application.route('/',methods = ['POST'])
 def hello():
+    top="Default"
+    if request.is_json:
+      result=request.get_json()
+      top=str(result.get("Topic"))
+
     credentials = pika.PlainCredentials(user,pas)
     parameters = pika.ConnectionParameters(service,5672,'/',credentials)
     connection = pika.BlockingConnection(parameters)
@@ -30,8 +33,8 @@ def hello():
     result = channel.queue_declare('', exclusive=True)
     queue_name = result.method.queue
 
-    binding_keys = ['anonymous.info']
-    
+    binding_keys = [top]
+
     if not binding_keys:
         sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
         sys.exit(1)
@@ -40,22 +43,21 @@ def hello():
         channel.queue_bind(
             exchange='topic_logs', queue=queue_name, routing_key=binding_key)
 
-   
-
-
     def callback(ch, method, properties, body):
         global mi
         print(" [x] %r:%r" % (method.routing_key, body))
         mi=body
         connection.close()
-    
+
     channel.basic_consume(
         queue=queue_name, on_message_callback=callback, auto_ack=True)
 
     channel.start_consuming()
-    return mi
+    return "Received Message :"+mi+" With Topic :"+binding_keys[0]
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=8080)
+
+      
 
     
